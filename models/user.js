c8bdf7695cf7b2f5e0cd1e2f821b8e3c05ee640b
Juan Jose Fajardo <jjftorrado@gmail.com>
2015-10-02 19:36:28 +0200

    Paso 23l. Tabla de usuarios

--- models/user.js
+++ models/user.js
@@ -45,10 +45,11 @@ module.exports = function( sequelize, DataTypes )
 		  	validate : { notEmpty: { msg: "-> Falta Password" }},
 		  	set      : function ( password )
 					  		{
-					  		var encripted = crypto
-					  						.createHmac( 'sha1', key )
-					  						.update( password )
-					  						.digest( 'hex' );
+					  		var encripted = password;
+					  		// var encripted = crypto
+					  		// 				.createHmac( 'sha1', key )
+					  		// 				.update( password )
+					  		// 				.digest( 'hex' );
 					  		// Evita passwords vacíos
 					  		if (password === '') encripted = '';
 					  		this.setDataValue( 'password', encripted );
@@ -66,10 +67,11 @@ module.exports = function( sequelize, DataTypes )
 	      	verifyPassword: function ( password )
 	      		{
 	      		// encripta el password introducido
-	      		var encripted = crypto
-		  						.createHmac( 'sha1', key )
-		  						.update( password )
-		  						.digest( 'hex' );
+	      		var encripted = password;
+	      		// var encripted = crypto
+		  					// 	.createHmac( 'sha1', key )
+		  					// 	.update( password )
+		  					// 	.digest( 'hex' );
 	      	  	return ( encripted === this.password );
 	      		}
 	    	}
